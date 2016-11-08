5da4e17debe3aeff1542aeb451aa45922b7fcbfb
Elvis <elvis.xiao@oceanwing.com>
2016-11-08 17:36:39 +0800

    save

--- routes/ui/index.js
+++ routes/ui/index.js
@@ -12,7 +12,7 @@ var cssRoutes = ['normalize', 'base', 'animation', 'icon'];
 var jsRoutes = ['ajax', 'algorithm', 'date', 'localStorage', 'security', 'dojo', 'number', 'csv', 'location'];
 
 //组件相关 ---
-var componentRoutes = ['buSelect', 'dialog', 'dropdown', 'fileView', 'imageCrop', 'select', 'table', 'form', 'tree', 'treeSelect', 'ui', 'uploader'];
+var componentRoutes = ['buSelect', 'dialog', 'dropdown', 'fileView', 'imageCrop', 'select', 'table', 'slide', 'autoComplete', 'form', 'tree', 'treeSelect', 'other', 'uploader'];
 
 cssRoutes.concat(jsRoutes).concat(componentRoutes).map(function(one) {
 	router.get('/' + one, function(req, res, next) {
