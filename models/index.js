c4dbd29e4d05b218a1e426c1ea2d75f18d7746c8
elvisxiao <elvis.xiao@oceanwing.com>
2016-12-02 13:17:55 +0800

    save

--- models/index.js
+++ models/index.js
@@ -4,8 +4,9 @@ if(!config || !config.db || !config.db.host || !config.db.port) {
 	console.error('配置文件错误，未找到数据库位置');
 	throw new Error('配置文件错误，未找到数据库位置');
 }
-
-var db = mongoose.createConnection('mongodb://' + config.db.user + ':' + config.db.pwd + '@' + config.db.host + ':' + config.db.port + '/' + config.db.db);
+var connectionString = 'mongodb://' + config.db.user + ':' + config.db.pwd + '@' + config.db.host + ':' + config.db.port + '/' + config.db.db;
+console.log('mongo connectionString:' + connectionString);
+var db = mongoose.createConnection(connectionString);
 
 exports.User = db.model('User', require('./user'));
 exports.Lucky = db.model('Lucky', require('./lucky'));
